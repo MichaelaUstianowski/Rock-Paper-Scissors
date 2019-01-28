545bb5354ff311bbdaf18912cfad11e2d8a87b0e
MichaelaUstianowski <44266495+MichaelaUstianowski@users.noreply.github.com>
2019-01-28 21:07:22 +0000

    Game functionality is done for now. Added some extra features.

--- js/scripts.js
+++ js/scripts.js
@@ -3,84 +3,98 @@
 //equivalent to jQuery document.ready
 window.addEventListener("load", () => {
 
+//COMPUTER'S CHOICE
+  //Generates a number between 1 and 3. This is the computer's choice.
+  function compChoiceGenerator(){
+    compChoice = Math.floor(Math.random()*(3-1+1)+1)
+  };
 
-//Getting buttons to assign a value when being pressed/player choice of hand
-var playerChoice = document.getElementById("R").addEventListener("click", function(){
-  let playerChoice = 1;
-  console.log(playerChoice);
+  //This will print the computer's choice onto the page.
+  function printCompChoice(){
+    if(compChoice == 1){
+      document.getElementById('printCompChoice').innerHTML = 'Rock';
+    }else if(compChoice == 2){
+      document.getElementById('printCompChoice').innerHTML = 'Paper';
+    }else
+    document.getElementById('printCompChoice').innerHTML = 'Scissors';
+    };
+
+//SCOREBOARD
+  //Declaring the scores for the scoreboard
+  var playerScore = 0;
+  var compScore = 0;
+
+  //Increase scoreboard functions for both player and computer.
+  function increasePlayerScore() {
+    playerScore++;
+    document.getElementById('playerscore').innerHTML = playerScore;
+  };
+  function increaseCompScore(){
+    compScore++;
+    document.getElementById('compscore').innerHTML = compScore;
+  };
+
+  //This will reset the scoreboard to 0 and reset the player and computer choices.
+    document.getElementById("reset").addEventListener("click", function(){
+      playerScore = 0;
+      compScore = 0;
+      document.getElementById('playerscore').innerHTML = playerScore;
+      document.getElementById('compscore').innerHTML = compScore;
+      document.getElementById('outcome').innerHTML = 'outcome';
+      document.getElementById('printCompChoice').innerHTML = 'Computer Choice';
+      document.getElementById('printPlayerChoice').innerHTML = 'Player Choice';
+    });
+
+//COMPARING PLAYER AND COMPUTER CHOICES
+  //Determines the winner of the game by comparing the player's and the computer's choices
+    function determineWinner(playerChoice){
+      if(playerChoice==compChoice){
+      document.getElementById('outcome').innerHTML ="DRAW!";
+      }else if(playerChoice == 1 && compChoice == 2){
+        document.getElementById('outcome').innerHTML ="COMP WINS!";
+        increaseCompScore();
+      }else if(playerChoice == 1 && compChoice == 3){
+        document.getElementById('outcome').innerHTML ="PLAYER WINS!";
+        increasePlayerScore();
+      }else if(playerChoice == 2 && compChoice == 1){
+        document.getElementById('outcome').innerHTML ="PLAYER WINS!";
+        increasePlayerScore();
+      }else if(playerChoice == 2 && compChoice == 3){
+        document.getElementById('outcome').innerHTML ="COMP WINS!";
+        increaseCompScore();
+      }else if(playerChoice == 3 && compChoice == 1){
+        document.getElementById('outcome').innerHTML ="COMP WINS!";
+        increaseCompScore();
+      }else if(playerChoice == 3 && compChoice == 2){
+        document.getElementById('outcome').innerHTML ="PLAYER WINS!";
+        increasePlayerScore();
+      }
+    }
+
+//PLAYER CHOICE & GAMEPLAY.
+    //Each button below are the options for the player. The game begins when the player selects and option.
+
+//Player selects 'ROCK' = playerChoice 1
+document.getElementById("R").addEventListener("click", function(){
+document.getElementById('printPlayerChoice').innerHTML = 'Rock';
+compChoiceGenerator();
+printCompChoice();
+determineWinner(1);
 });
 
-var playerChoice = document.getElementById("P").addEventListener("click", function(){
-  let playerChoice = 2;
-  console.log(playerChoice);
+//Player selects 'PAPER' = playerChoice 2
+document.getElementById("P").addEventListener("click", function(){
+  document.getElementById('printPlayerChoice').innerHTML = 'Paper';
+  compChoiceGenerator();
+  printCompChoice();
+  determineWinner(2);
 });
 
-var playerChoice = document.getElementById("S").addEventListener("click", function(){
-  let playerChoice = 3;
-  console.log(playerChoice);
-});
-
-// Var compChoice;
-
-// Function of the computers choice
-// Randomly picks r, p or s
-// Set compChoice to the computers choice
-//
-// Function get randomchoice() {
-  // 	Get a random number and assign it to a value for comp choice
-  // }
-  //
-  // Value between 0 and 1
-  // If num between 0 and 0.33 = rock
-  // Else if num more than 0.33 and less than or equal to 0.67 = paper
-  // Else = scissors
-  //
-var compChoice = document.querySelector("#R").addEventListener("click", function(){
-   let compChoice = Math.floor(Math.random()*(3-1+1)+1);
-     console.log(compChoice);
-});
-
-var compChoice = document.querySelector("#P").addEventListener("click", function(){
-   let compChoice = Math.floor(Math.random()*(3-1+1)+1);
-     console.log(compChoice);
-});
-
-var compChoice = document.querySelector("#S").addEventListener("click", function(){
-   let compChoice = Math.floor(Math.random()*(3-1+1)+1);
-     console.log(compChoice);
-});
-
-if(playerChoice==compChoice){
-  console.log('draw');
-  // document.getElementById('outcome').innerHTML ="DRAW!";
-};
-
-// if playerChoice==compChoice => DRAW
-// if playerChoice = 1 compChoice = 2 => COMP wins
-// if playerChoice = 1 compChoice = 3 => PLAYER wins
-// if playerChoice = 2 compChoice = 1 => PLAYER wins
-// if playerChoice = 2 compChoice = 3 => COMP wins
-// if playerChoice = 3 compChoice = 1 => PLayer wins
-// if playerChoice = 3 compChoice = 2 => COMP wins
-
-
-// Var playerScore = 0;
-
-
-
-
-// Increase the winners score
-//
-// Function increasePlayerScore() {
-// 	Increase the payers score by 1;
-// }
-//
-// Function increaseCompScore() {
-// Increase comp score by 1
-// }
-//
-// Function restScoreBoard() {
-// 	Set both scores back to 0
-// }
-
+//Player selects 'Scissors' = playerChoice 3
+document.getElementById("S").addEventListener("click", function(){
+  document.getElementById('printPlayerChoice').innerHTML = 'Scissors';
+  compChoiceGenerator();
+  printCompChoice();
+  determineWinner(3);
+  });
 });
